5d9b69770ccee3e276a06b567468270740625ac7
mainkunalhu <natwarlaluzumaki@gmail.com>
2025-11-01 13:09:33 +0530

    new theme and nvim tree

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", function()
-    vim.cmd("Neotree")
+	vim.cmd("NvimTreeOpen")
 end)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,6 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.shiftwidth = 4
 vim.opt.relativenumber = true
-
-
-
-
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
